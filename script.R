2240fe155d78e600b583eebd032af64bf34b134d
Carmine Apice <carmineapice@gmail.com>
2024-09-22 21:09:25 +0200

    Update script.R

--- script.R
+++ script.R
@@ -1,33 +1,38 @@
 
-# Funzione. Rappresenta la derivata prima di Revenue - Cost, quindi segue
-# regola MR = MC
-function_1 <- function(q) {
+# The Objective function
+objective_fun <- function(q) {
   return (100*q-q^2-4*q)
 }
 
-# Test: La funzione calcolata a mano dava Q* = 48
-function_1(48)
+# Test of the function
+objective_fun(48)
 
-# Loop che serve per valutare tra tutti i possibili valori della Q di input
-# quale massimizza il valore della funzione stessa.
+# For Loop to check for each possible value of "Q" (up to 100)
+# which is the maximum value of our objective function. Results are
+# stored in x and y in order to plot them (see below)
+
 x <- c()
 y <- c()
 
 for (q in 1:100) {
   r1 <- q
-  r2 <- print(function_1(q))
+  r2 <- print(objective_fun(q))
   
   x <- c(x, r1)
   y <- c(y, r2)
   
 }
+
+# Check among the results which is the one that returns 
+# tha maximum value.
 which.max(y)
 
+# Graphical Inspection
 plot(x,y)
 abline(v=48, col="red")
 
 
-
+# Check with "optimize" function in R
 f <- function (q) (100*q-q^2)-(4*q)
 xmax <- optimize(f, c(0, 1000), tol = 0.0001, maximum = TRUE)
 xmax
